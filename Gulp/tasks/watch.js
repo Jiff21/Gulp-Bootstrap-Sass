7d432760ed7953f555e5f85391c242ccbdf8fffa
Jeff <Jeff@bynd.com>
2014-07-30 09:17:13 -0700

    added a watch for views.

--- Gulp/tasks/watch.js
+++ Gulp/tasks/watch.js
@@ -6,6 +6,7 @@ gulp.task('watch', ['serve'], function () {
 
 	// watch for changes
 	gulp.watch(['app/*.html'], reload);
+	gulp.watch(['app/views/*.html'], reload);
 
 	gulp.watch('app/styles/**/*.scss', ['styles']);
 	gulp.watch('app/scripts/**/*.js', ['scripts']);
