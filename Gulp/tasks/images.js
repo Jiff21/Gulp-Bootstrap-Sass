d53b3588efbd0f830c385114c350fabc73200817
Jeff <Jeff@bynd.com>
2014-07-30 09:22:42 -0700

    Added reload to images task.

--- Gulp/tasks/images.js
+++ Gulp/tasks/images.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('images', function () {
 	return gulp.src('app/images/**/*')
