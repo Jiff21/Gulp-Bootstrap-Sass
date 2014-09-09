3aa6e5c98b2cc266b680458881f598ffd75f57c5
Jeffrey P Campbell <inajiff@gmail.com>
2014-09-08 22:43:42 -0700

    Changed to main-bower-files to fix depreciated issue. Uninstalled Streamqueue and gulp-bower-files

--- Gulp/tasks/fonts.js
+++ Gulp/tasks/fonts.js
@@ -1,12 +1,8 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('fonts', function () {
-	var streamqueue = require('streamqueue');
-	return streamqueue({objectMode: true},
-		$.bowerFiles(),
-		gulp.src('app/fonts/**/*')
-	)
+gulp.task('fonts', function() {
+	return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
 		.pipe($.filter('**/*.{eot,svg,ttf,woff}'))
 		.pipe($.flatten())
 		.pipe(gulp.dest('dist/fonts'))
